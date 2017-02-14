d248f69ef884d5a43392e32e0a22f047a0a1652b
mirko <mirko@faina.faina>
2017-02-14 19:01:24 +0100

    ascii

---
+++ ascii/ascii.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__module_name__ = "Ascii"
+__module_version__ = "beta"
+__module_author__ = "Mroik"
+__module_description__ = "Imports ascii art from https://github.com/Mroik/myascii.git"
+
+import xchat
+import string
+import os
+
+def coloring(text):
+    text = text.replace("+",chr(3)+"9,9+")
+    text = text.replace("-",chr(3)+"1,1-")
+    xchat.command("say "+text)
+
+def update(word,word_eol,userdata):
+    os.system("git clone https://github.com/Mroik/myascii.git")
+    print("Ascii database updated")
+    return xchat.EAT_ALL
+xchat.hook_command("ASCUP",update)
+
+def ascii(word,word_eol,userdata):
+    text = open("/home/mirko/myascii/"+word[1]+".txt")
+    for line in text:
+        xchat.command("say "+chr(3)+"9,1"+line)
+    text.close
+    return xchat.EAT_ALL
+xchat.hook_command("ASCII",ascii)
+
+def ascii2(word,word_eol,userdata):
+    text = open("/home/mirko/myascii/"+word[1]+".txt")
+    for line in text:
+        line = line.replace("+",chr(3)+"9,9+")
+        line = line.replace("-",chr(3)+"1,1-")
+        xchat.command("say "+chr(3)+"9,1"+line)
+    text.close
+    return xchat.EAT_ALL
+xchat.hook_command("ASCIIC",ascii2)
+
+print("Ascii script loaded")
