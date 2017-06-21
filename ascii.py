d839e054056fddef248a013397366af537946a57
mirko <mirko@faina.faina>
2017-06-21 21:58:50 +0200

    New script

--- ascii.py
+++ ascii.py
@@ -15,6 +15,16 @@ def coloring(text):
     text = text.replace("-",chr(3)+"1,1-")
     xchat.command("say "+text)
 
+def asciilist(word,word_eol,userdata):
+    lista=os.listdir("/home/mirko/myascii/ascii")
+    for x in lista:
+        print(x)
+    lista=os.listdir("/home/mirko/myascii/ascii2")
+    for x in lista:
+        print(x)
+    return xchat.EAT_ALL
+xchat.hook_command("ASCLIST",asciilist)
+
 def update(word,word_eol,userdata):
     os.system("git clone https://github.com/Mroik/myascii.git")
     print("Ascii database updated")
@@ -22,10 +32,13 @@ def update(word,word_eol,userdata):
 xchat.hook_command("ASCUP",update)
 
 def ascii(word,word_eol,userdata):
-    text = open("/home/mirko/myascii/ascii/"+word[1]+".txt")
-    for line in text:
-        xchat.command("say "+chr(3)+"9,1"+line)
-    text.close
+    if os.path.exists("/home/mirko/myascii/ascii/"+word[1]+".txt"):
+        text = open("/home/mirko/myascii/ascii/"+word[1]+".txt")
+        for line in text:
+            xchat.command("say "+chr(3)+"9,1"+line)
+        text.close
+    else:
+        print("This file doesn't exist")
     return xchat.EAT_ALL
 xchat.hook_command("ASCII",ascii)
 
